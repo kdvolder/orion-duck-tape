0cd9581c1a1bcd2c0680daccab78d09e60fa6f4f
Kris De Volder <kdevolder@pivotal.io>
2016-08-18 09:41:15 -0700

    Fixup defaults.pref by having duck-tape app serve its own.

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 var port = 9000;
 var orion = require('orion');
 var express = require('express');
-var myapp = express()
-            .use('/ide', orion({ workspaceDir: process.env.HOME }))
+var app = express();
+var path = require('path');
 
-myapp.listen(port, function () {
+app.get('/ide/defaults.pref', function(req, res) {
+  var df = path.join(__dirname, 'resources/defaults.pref');
+  console.log('sending file: "'+df+"'");
+  res.sendFile(df);
+});
+
+app.use('/ide', orion({ workspaceDir: process.env.HOME }))
+
+app.listen(port, function () {
   console.log('Example app listening on port '+port+'!');
 });
